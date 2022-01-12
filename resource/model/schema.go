d392314589d23915d78f73c20f3e81181a89dbae
allenzhli <allenzhli@tencent.com>
2022-01-12 18:10:21 +0800

    add resource schema short name

--- resource/model/schema.go
+++ resource/model/schema.go
@@ -5,11 +5,12 @@ import (
 )
 
 type ResourceSchema struct {
-	Name       TypeName
-	Brief      string
-	Prefix     string
-	UrnPattern string // UrnPattern is a NamedRegex that can be used transform urn from attributes.
-	Attributes []*AttributeSchema
+	Name         TypeName
+	Brief        string
+	Prefix       string
+	UrnPattern   string // UrnPattern is a NamedRegex that can be used transform urn from attributes.
+	ShortPattern string
+	Attributes   []*AttributeSchema
 }
 
 type AttributeSchema struct {
